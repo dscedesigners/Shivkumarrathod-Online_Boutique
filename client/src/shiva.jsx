00cbb816e0ff034dfef15147c07a55b2686e922e
Shivkumarrathod <137758681+Shivkumarrathod@users.noreply.github.com>
2024-10-07 20:02:50 +0530

    chck

--- client/src/shiva.jsx
+++ client/src/shiva.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const shiva = () => {
   return (
-    <div>shiva</div>
+    <div>
+      sjdfudduf
+      shhfufbrjf
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam assumenda distinctio corrupti illo tenetur doloremque mollitia consequuntur nulla! Sunt asperiores architecto quod ipsa delectus eaque quia minima beatae consectetur error.
+    </div>
+
   )
 }
 
